4310992a7415a24b7c2cc9ba444cdbfe6fc66401
high54 <julien.bertacco@gmail.com>
2020-11-09 10:55:23 +0100

    Modification du routing et du menu

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'experiences'
+  },
+  {
+    path: 'experiences',
+    loadChildren: () => import('./modules/experiences/experiences.module').then((m) => m.ExperiencesModule)
+  },
+  {
+    path: 'skills',
+    loadChildren: () => import('./modules/skills/skills.module').then((m) => m.SkillsModule)
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./modules/projects/projects.module').then((m) => m.ProjectsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', initialNavigation: 'enabled' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
