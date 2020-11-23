1c8753c4568dd6600dd4cd69909a37280f7d6700
high54 <julien.bertacco@gmail.com>
2020-11-23 18:19:56 +0100

    Retrait des modules Material non utilisé

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -17,14 +15,12 @@ const materialModules: any[] = [
     MatButtonModule,
     MatDialogModule,
     MatDividerModule,
-    MatExpansionModule,
     MatIconModule,
     MatListModule,
     MatMenuModule,
     MatProgressBarModule,
     MatRadioModule,
     MatSidenavModule,
-    MatSlideToggleModule,
     MatSnackBarModule,
     MatToolbarModule
 ];
